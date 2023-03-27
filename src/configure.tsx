37e01e8676c0d4de904bf9a0d3f6fac44471d81b
verveguy <bpjadam@gmail.com>
2023-03-27 13:37:10 -0400

    Last piece of config change

--- src/configure.tsx
+++ src/configure.tsx
@@ -5,23 +5,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import ConfigurationPanel from "./Configuration";
+import ConfigurationPanel from "./ConfigurationPanel";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error('Failed to find the root element');
 
-/* 
-  style the injection in the way we want for the
-  purpose of being an "overlay" to the current website
-*/
-const globalStyles = document.createElement("style");
+// /* 
+//   style the injection in the way we want for the
+//   purpose of being an "overlay" to the current website
+// */
+// const globalStyles = document.createElement("style");
 
-globalStyles.innerHTML = `
-
-`;
+// globalStyles.innerHTML = `
+// `;
 
 // inject ourselves
-document.body.appendChild(globalStyles);
+// document.body.appendChild(globalStyles);
 
 const root = createRoot(rootElement);
 root.render(
